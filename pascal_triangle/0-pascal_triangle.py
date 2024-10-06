fba85a4b8fb95348c7b81b3a92a81025facf9436
IrutingaboRaissa <r.irutingab2alustudent.com>
2024-10-06 13:34:01 +0200

    paslcal's T

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -2,9 +2,10 @@
 # 12-pascal_triangle.py
 """A Python script that generates Pascal's Triangle."""
 
+
 def pascal_triangle(n):
     """Generates Pascal's triangle up to the nth row.
-    
+
     Args:
         n (int): Number of rows of Pascal's triangle to generate.
 
@@ -12,10 +13,10 @@ def pascal_triangle(n):
         list: A list of lists representing Pascal's triangle.
     """
     list_of_list = []
-    
+
     if n <= 0:
         return list_of_list
-    
+
     for i in range(n):
         if i == 0:
             list_of_list.append([1])
@@ -30,4 +31,3 @@ def pascal_triangle(n):
     return list_of_list
 
 # Add an empty line after the last line of code:
-
